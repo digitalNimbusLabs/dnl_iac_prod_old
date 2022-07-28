10cbe5d5c0b65a17a65225bfdecf61e8cfb4bf9a
digitalNimbusLabs <89986670+digitalNimbusLabs@users.noreply.github.com>
2022-07-28 11:33:36 -0500

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+    default = "AM"
+}
+
+
+variable "location" {
+    default = "EastUS"
+}
+
+
+variable "environment" {
+    default = "Dev"
+}
+
+
+variable "application" {
+    default = "capsule"
+}
+
+
+variable "app_abbreviation" {
+    default = "shp"
+}
+
+
+variable "name" {
+    default = "foundation"
+}
+
+
+variable "tags" {
+    default = {
+
+    }
+}
