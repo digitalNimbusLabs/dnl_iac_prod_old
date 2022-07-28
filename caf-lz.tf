ec2232c1a4cbadee134eb47f3ed1c3bdc2eae326
digitalNimbusLabs <89986670+digitalNimbusLabs@users.noreply.github.com>
2022-07-28 13:25:58 -0500

    Update caf-lz.tf

--- caf-lz.tf
+++ caf-lz.tf
@@ -22,6 +22,35 @@ module "enterprise_scale" {
   
   deploy_management_resources = true
   subscription_id_management  = data.azurerm_client_config.core.subscription_id
+ 
+
+
+}
+/*
+# You can use the azurerm_client_config data resource to dynamically
+# extract connection settings from the provider configuration.
+
+data "azurerm_client_config" "core" {}
+
+# Call the caf-enterprise-scale module directly from the Terraform Registry
+# pinning to the latest version
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "2.1.2"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = "dnl"
+  root_name      = "Digital Nimbus Labs"
+  
+  deploy_management_resources = true
+  subscription_id_management  = data.azurerm_client_config.core.subscription_id
   
   custom_landing_zones = {
     "${var.root_id}-prod" = {
@@ -47,3 +76,4 @@ module "enterprise_scale" {
   }
 
 }
+*/
