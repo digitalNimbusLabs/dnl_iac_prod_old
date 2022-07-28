5991f6466bc6939ca069af3bf5ca55bf506bab4a
digitalNimbusLabs <89986670+digitalNimbusLabs@users.noreply.github.com>
2022-07-28 13:52:37 -0500

    Update caf-lz.tf

--- caf-lz.tf
+++ caf-lz.tf
@@ -19,6 +19,7 @@ module "enterprise_scale" {
   root_parent_id = data.azurerm_client_config.core.tenant_id
   root_id        = "dnl"
   root_name      = "Digital Nimbus Labs"
+  library_path   = "${path.root}/lib"
   
   deploy_management_resources = true
   subscription_id_management  = data.azurerm_client_config.core.subscription_id
